a0042bd0eada895e4dbee7781986f062638fc1d7
Wayne Eaker <wayne@zengenuity.com>
2016-08-15 11:41:52 -0400

    Added jQuery code

--- bitwisdom/js/global.js
+++ bitwisdom/js/global.js
@@ -1,1 +1,20 @@
-console.log('global file loaded');
+(function ($, Drupal) {
+
+  'use strict';
+  
+  Drupal.behaviors.bitwisdomCountNodes = {
+    attach: function(context) {
+      console.log('global file loaded');
+      
+      var total = 0;
+      
+      $(context).find('article.node').each(function() {
+        total++;
+      });
+      
+      console.log('Found ' + total + ' nodes.');
+
+    }
+  };
+
+ })(jQuery, Drupal);
